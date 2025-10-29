641c80c4e8c055a67120f3242d0dcca18b5aa565
mahmoud-mumdouh31 <132226229+mahmoud-mumdouh31@users.noreply.github.com>
2025-10-29 14:51:05 +0300

    Update ProjectEstimation.cs

--- Domain/Entities/Project/ProjectEstimation.cs
+++ Domain/Entities/Project/ProjectEstimation.cs
@@ -3,19 +3,18 @@
 public class ProjectEstimation
 {
     public int EstimationId { get; set; }
-    public int ProjectId { get; set; }
-    public Projects Project { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
 
-
-    public decimal FixedAssetsPercent { get; set; } // 0 or 5
-    public decimal RawMaterialsPercent { get; set; }
-    public decimal LaborPercent { get; set; } = 4;
-    public decimal IncentivesPercent { get; set; } = 10;
-    public decimal OperatingExpensesPercent { get; set; } = 65;
-    public decimal EquipmentDepreciationPercent { get; set; } = 2; 
-    public decimal AccommodationRecoveryPercent { get; set; } = 2; 
-    public decimal AdministrativeExpensesPercent { get; set; } = 2; 
-    public decimal InsurancePercent { get; set; } = 0; 
-    public decimal GeneralSurplusPercent { get; set; } = 10; 
+        public decimal FixedAssetsPercent { get; set; } // 0 or 5
+        public decimal RawMaterialsPercent { get; set; }
+        public decimal LaborPercent { get; set; } = 4;
+        public decimal IncentivesPercent { get; set; } = 10;
+        public decimal OperatingExpensesPercent { get; set; } = 65;
+        public decimal EquipmentDepreciationPercent { get; set; } = 2; 
+        public decimal AccommodationRecoveryPercent { get; set; } = 2; 
+        public decimal AdministrativeExpensesPercent { get; set; } = 2; 
+        public decimal InsurancePercent { get; set; } = 0; 
+        public decimal GeneralSurplusPercent { get; set; } = 10; 
 
 }
