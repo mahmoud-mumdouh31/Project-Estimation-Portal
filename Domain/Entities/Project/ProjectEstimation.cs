12935bf6c5789efac63d64ca7a45880da7ce34e1
mahmoud-mumdouh31 <132226229+mahmoud-mumdouh31@users.noreply.github.com>
2025-10-29 14:25:42 +0300

    Update ProjectEstimation.cs

--- Domain/Entities/Project/ProjectEstimation.cs
+++ Domain/Entities/Project/ProjectEstimation.cs
@@ -7,13 +7,11 @@ public class ProjectEstimation
     public Projects Project { get; set; }
 
 
-    // Percent values (0-100)
     public decimal FixedAssetsPercent { get; set; } // 0 or 5
     public decimal RawMaterialsPercent { get; set; }
     public decimal LaborPercent { get; set; } = 4;
     public decimal IncentivesPercent { get; set; } = 10;
     public decimal OperatingExpensesPercent { get; set; } = 65;
-    // البنود الإضافية المطلوبة
     public decimal EquipmentDepreciationPercent { get; set; } = 2; 
     public decimal AccommodationRecoveryPercent { get; set; } = 2; 
     public decimal AdministrativeExpensesPercent { get; set; } = 2; 
